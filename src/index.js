54fb9de906e40514866d6b74ad4ec026d7f3623a
Huy Nguyen <huyb.1991@gmail.com>
2018-05-31 22:01:19 +0700

    Add html-webpack-plugin and index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("react boilerplate");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello React!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
