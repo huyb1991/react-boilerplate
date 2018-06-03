945859ac4a4853ef9a7a2d91ec1cb41d81ec82ec
Huy Nguyen <huyb.1991@gmail.com>
2018-06-03 11:48:54 +0700

    Update ESLint rules and test Button component

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import styled, { css } from 'styled-components'
-
-const Button = styled.button`
-  border-radius: 3px;
-  padding: 0.25em 1em;
-  margin: 0 1em;
-  background: transparent;
-  color: palevioletred;
-  border: 2px solid palevioletred;
-
-  ${props => props.primary && css`
-    background: palevioletred;
-    color: white;
-  `}
-`
+// Components
+import Button from './components/Button'
 
 const App = () => (
   <div>
     <p>Hello React!</p>
     <Button>Normal Button</Button>
-    <Button primary>Primary Button</Button>
+    <Button type="primary">Primary Button</Button>
   </div>
 )
 
