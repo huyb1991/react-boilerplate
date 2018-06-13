aba193d64f87fed3815f51fa34a8fb6124a77e8a
Huy Nguyen <huyb.1991@gmail.com>
2018-06-13 17:03:54 +0700

    Refactor coding style with ES6

--- src/containers/HomePage/saga.js
+++ src/containers/HomePage/saga.js
@@ -1,7 +1,6 @@
 /**
  * Gets the repositories of the user from Github
  */
-
 import { call, put, takeLatest } from 'redux-saga/effects'
 import { LOAD_REPOS } from './constants'
 import { reposLoaded, repoLoadingError } from './actions'
@@ -12,12 +11,11 @@ import request from 'helpers/request'
  * Github repos request/response handler
  */
 export function* getRepos() {
-  // Select username from store
   const username = 'huyb1991'
   const requestURL = `https://api.github.com/users/${username}/repos?type=all&sort=updated`
 
   try {
-    // Call our request helper (see 'utils/request')
+    // Call our request helper (see 'helpers/request')
     const repos = yield call(request, requestURL)
     yield put(reposLoaded(repos, username))
   } catch (err) {
