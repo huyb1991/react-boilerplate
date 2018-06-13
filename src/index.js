8a219d73cd98a0db3be9144eb17f2f0d100504a9
Huy Nguyen <huyb.1991@gmail.com>
2018-06-13 16:37:50 +0700

    Inject global css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { injectGlobal } from 'styled-components'
 
 // Configs
 import store from './store'
@@ -13,6 +14,31 @@ import Header from './components/Header'
 import HomePage from './containers/HomePage'
 import AboutPage from './containers/AboutPage'
 import NotFoundPage from './containers/NotFoundPage'
+
+// Themes
+import { COLORS } from 'themes'
+
+// eslint-disable-next-line
+injectGlobal`
+  body {
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    line-height: 1.15;
+    font-weight: 400;
+    font-size: 1rem;
+    background-color: #f6f6f6;
+    color: ${COLORS.dark};
+  }
+
+  a {
+    text-decoration: none;
+    color: ${COLORS.primary};
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
 
 const App = () => (
   <Provider store={store}>
